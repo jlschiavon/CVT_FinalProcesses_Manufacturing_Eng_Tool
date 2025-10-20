c37651dd80cb48c6bb4f142b5ac8cbfea853b820
Jorge Schiavon <105507250+jlschiavon@users.noreply.github.com>
2025-10-19 21:18:24 -0600

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,11 @@
+from datetime import datetime
+import pandas as pd
 
+def asignar_turno(ts):
+    h = ts.hour + ts.minute / 60
+    if 7 <= h < 15:
+        return "1st Shift"
+    elif 15 <= h < 22.5:
+        return "2nd Shift"
+    else:
+        return "3rd Shift"
